bb8528713044a71217d64270a3d32e77029c82c8
swayam45-wq <patelswayam88@gmail.com>
2025-09-19 21:01:45 -0500

    Update main.c
    Added some comments and removed some unnecessary comments that I wrote during code for my understanding.

--- main.c
+++ main.c
@@ -20,7 +20,6 @@ typedef struct cell_struct{
 //return false if input rule is invalid
 bool setValArray(int valArray[7], int rule) {
 
-        //TODO: Task 1 - write the setValArray() function
         while (rule >= 0 && rule <= 2186){
             int currNum = rule;
             for (int i = 0; i <7; i++) {
@@ -49,10 +48,7 @@ void setSums(cell world[WORLD_SIZE]) {
         else {
             world[i].localSum = world[i].status + world[i-1].status + world[i+1].status;
         }
-
     }
-    //TODO: Task 4 - write the setSums() function
-
     return;
 }
 
@@ -99,31 +95,10 @@ int main() {
         printf("%d", firstArr[i]);
     }
     printf("\n");
-
-
-/*    while (ans) {
-        if (ans) {
-            printf("The value array for rule #%d is ", starterNum);
-            for (int i = 6; i >= 0; i--) {
-                printf("%d", firstArr[i]);
-            }
-            printf("\n"); 
-        }
-            else if (ans == false) {
-                printf("Enter the rule # (0-2186): \n");
-                scanf("%d", &starterNum);
-                bool ans = setValArray(firstArr , starterNum);
-                printf("The value array for rule #%d is ", starterNum);
-            }
-    }*/
     
-
-    //TODO: Task 2 - read in a valid rule#, allowing repeated attempts if necessary,
     //      generate the rule's 7-trit (i.e. 7 base-3 digits) status value array,  
     //      print the status value array in correct ternary (i.e. base-3) number order
     
-
-
     printf("The evolution of all possible states are as follows:\n");
     printf("Local Sum:   6       5       4       3       2       1       0      \n");
     printf("States:    |+++|   |++-|   |++ |   |+- |   |-- |   |-  |   |   |    \n");
@@ -156,11 +131,9 @@ int main() {
         }
 
     }   
-    //TODO: Task 3 - use the rule status value array to report the evolution 
+
     //      step for all possible cell neighbor states [left, me, right].
     //      Follow the format, including whitespaces, of the sample output EXACTLY.
-    
-
     int gen;
     printf("Enter the number of generations (1-49): \n");
     scanf("%d", &gen);
@@ -170,8 +143,6 @@ int main() {
         scanf("%d", &gen);
     }
 
-
-    //TODO: Task 5a - read in the total number of generation evolution steps
     //      from the user, allowing repeated attempts for invalid values.
     //      Follow the format, including whitespaces, of the sample output EXACTLY.
     int initial;
@@ -183,7 +154,6 @@ int main() {
         printf("Enter the value (1 or 2) for the initial active cell: \n");
         scanf("%d", &initial);
     }
-    //TODO: Task 5b - read in the initial status value for the middle cell, 
     //      allowing repeated attempts for invalid values. 
     //      Follow the format, including whitespaces, of the sample output EXACTLY.
     for (int i = 0; i<WORLD_SIZE; i++) {
@@ -204,15 +174,12 @@ int main() {
     //world[WORLD_SIZE/2].status = 
     printf("Initializing world & evolving...\n");
     printf("                                %c                                 %d ", initialSign, initial);
-    //TODO: Task 5c - initialize the world with the ONLY non-zero cell in the 
     //      exact middle of the world, whose status value is set using the 
     //      scanned-in value from the user above. Make sure to also set the 
     //      localSum subitem for each cell of the initial world.
     //      Follow the format, including whitespaces, of the sample output EXACTLY.
          
 
-
-    //TODO: Task 7 - evolve the world the user-specified number of generations,
     //      printing the world after each evolution step, where each cell prints as
     //      '+', '-', or ' ' (whitespace) for status value of 2, 1, or 0, respectively.
     //      After the world display, also print out the total sum of all status values.
@@ -276,28 +243,7 @@ int main() {
             printf("%d", ones);
         }
     }
-
-    //TODO: Task 8 - after the line under the last generation, 
-    //      print the total sum for each cell throughout all evolution steps vertically.
-    //      The cell total sums should be printed as two digit numbers across two lines.
-    //      Single-digit total sums should be represented as a whitespace on the top line, 
-    //      and the single-digit on the lower line. 
-    //      For total sums of zero, print both digits as whitespaces.
-    //      Follow the format, including whitespaces, of the sample output EXACTLY.
-    //
-    //      Ex: consider a world after many evolution steps with the following final generation:
-    //          display-> ++--+-    -+--++---  --+--+-  -+-  -+--+--  ---++--+-    -+--++  64 <-display
-    //          display->_________________________________________________________________<-display   
-    //          display->            11111111122121222233233222212122111111111            <-display 
-    //          display-> 243666698963456647182091851471191174158190281746654369896666342 <-display 
-    //                   ^ ^                             ^
-    //      note: the first cell was ALWAYS 0, so its total sum is zero, and prints as two whitespaces
-    //            the third cell has a total sum of 4, so it prints as a whitespace above the digit 4
-    //            the middle cell has a total sum of 29, so it prints the digit 2 above the digit 9
-
-    
-    
-    printf("\n"); // make sure to end your program with a newline
-
+  
+    printf("\n"); 
     return 0;
 }
